6fd68ff3454cb37b56260c1ccab529cb5677ce60
Enes <57236750+Enessolmaz@users.noreply.github.com>
2022-10-31 01:47:07 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,8 @@
 import * as React from "react";
 import Categories from "./Categories";
-
-
 import MobileApp from "./MobileApp";
 import Slide from "./Slide";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PageCategories from "./PageCategories";
+
 
 function Home() {
   return (
