283b50423905e6f7b62fd60ebed925b29ba10b9c
Enes <57236750+Enessolmaz@users.noreply.github.com>
2022-11-16 14:32:38 +0300

    Update ProductPage.jsx

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -24,7 +24,7 @@ function ProductPage({ showProduct }) {
                 alt="none"
                 srcSet={product.img}
                 style={{
-                      width: "14vw",
+                      width: "11vw,
                       minWidth: "125px",
                       border: "2px solid rgb(203, 189, 255)",
                       borderRadius: "10px",
